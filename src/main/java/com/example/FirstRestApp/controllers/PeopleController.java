aaa89590c62f84fef806421dd96077d85a5fa17f
Aleksandr Guido <iggelds@gmail,com>
2022-12-29 14:54:40 +0100

    Initial commit

---
+++ src/main/java/com/example/FirstRestApp/controllers/PeopleController.java
@@ -0,0 +1,34 @@
+package com.example.FirstRestApp.controllers;
+
+import com.example.FirstRestApp.models.Person;
+import com.example.FirstRestApp.services.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping
+    public List<Person> getPeople() {
+        return peopleService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") int id) {
+        return peopleService.findOne(id);
+    }
+}
